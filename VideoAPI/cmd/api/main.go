8e8937bdeb95092a31815cacd65b52662fb984d3
DexScen <dexscen@gmail.com>
2024-11-20 18:18:37 +0500

    alternated logger

--- VideoAPI/cmd/api/main.go
+++ VideoAPI/cmd/api/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"log/slog"
+	"os"
+	"time"
 
 	"github.com/DexScen/VideoBot/VideoAPI/pkg/utils"
 )
 
 func main() {
-	err := utils.NewSlog()
+	f, err := os.OpenFile("VideoAPI\\pkg\\utils\\videoapi.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
 	if err != nil {
-		panic("logger config failure")
+		panic("file open failure")
 	}
+	defer f.Close()
+	utils.NewSlog(f)
+
 	slog.Info("Api started working")
+
+	for {
+		slog.Info("Api started working")
+		time.Sleep(5 * time.Second)
+	}
 }
--- VideoAPI/pkg/utils/slogger.go
+++ VideoAPI/pkg/utils/slogger.go
@@ -6,20 +6,15 @@ import (
 	"time"
 )
 
-func NewSlog() error{
-	f, err := os.OpenFile("VideoAPI\\pkg\\utils\\videoapi.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
-	if err != nil{
-		return err
-	}
+func NewSlog(f *os.File) {
 	logger := slog.New(slog.NewJSONHandler(f,
 		&slog.HandlerOptions{
 			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 				if a.Key == slog.TimeKey {
-					a.Value = slog.StringValue(time.Now().Format("yyyy-MM-dd HH:mm:ss.SSS"))
+					a.Value = slog.StringValue(time.Now().Format("2006-01-02 15:04:05.000"))
 				}
 				return a
 			},
 		}))
 	slog.SetDefault(logger)
-	return nil
 }
