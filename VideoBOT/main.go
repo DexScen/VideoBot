c213926dd3d9389b69cb8531f34c86cf59d2b2e7
j0n1que <Yevgeniyshc@mail.ru>
2024-11-30 23:04:31 +0300

    added client for sso service

--- VideoBOT/main.go
+++ VideoBOT/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
 	"os"
+	"strconv"
+	"time"
 
+	ssogrpc "github.com/DexScen/VideoBot/VideoBOT/internal/clients/sso/grpc"
 	"github.com/DexScen/VideoBot/VideoBOT/model"
 	msghandlers "github.com/DexScen/VideoBot/VideoBOT/msgHandlers"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -11,7 +16,7 @@ import (
 )
 
 func init() {
-	//conf.env contains TELEGRAM_APITOKEN=yourtoken
+	//conf.env contains TELEGRAM_APITOKEN=yourtoken and info for sso service
 
 	envFile := "videoBOT/conf.env"
 
@@ -22,6 +27,9 @@ func init() {
 
 func main() {
 	token, exists := os.LookupEnv("TELEGRAM_APITOKEN")
+
+	var logger *slog.Logger
+
 	if !exists {
 		log.Println("cant find token")
 	}
@@ -30,14 +38,48 @@ func main() {
 		log.Panic(err)
 	}
 
+	addr, exists := os.LookupEnv("SSO_ADDRESS")
+	if !exists {
+		log.Println("cant find address for sso service")
+	}
+
+	timeoutString, exists := os.LookupEnv("SSO_TIMEOUT")
+	if !exists {
+		log.Println("cant find timeout for sso service")
+	}
+
+	timeout, _ := time.ParseDuration(timeoutString)
+
+	retriesCountString, exists := os.LookupEnv("SSO_RETRIESCOUNT")
+	if !exists {
+		log.Println("cant find retrise count for sso service")
+	}
+
+	retriesCount, _ := strconv.Atoi(retriesCountString)
+
 	bot.Debug = true // false if no need
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+	ssoClient, err := ssogrpc.New(
+		context.Background(),
+		logger,
+		addr,
+		timeout,
+		retriesCount,
+	)
+	if err != nil {
+		log.Println("failed to init sso client", err)
+		os.Exit(1)
+	}
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
+
+	//Pass ssoClient wherever you want, example of usage: 	err := ssoClient.RegisterNewUser(ctx, login, password, telegramLogin)...
+
+	ssoClient.GetAllUsers(context.Background())
 
 	for update := range updates {
 		if update.Message == nil { // ignore non-Message updates
@@ -51,45 +93,45 @@ func main() {
 				log.Panic(err)
 			}
 		}
-		
+
 		if update.Message.Text == "Get video" {
 			err := msghandlers.HandleGetVideo(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
 
 		if update.Message.Text == "Register" {
 			err := msghandlers.HandleRegister(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
 
 		if update.Message.Text == "Log in" {
 			err := msghandlers.HandleLogIn(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
 
 		if update.Message.Text == "Change password" {
 			err := msghandlers.HandleChangePassword(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
 
 		if update.Message.Text == "Delete video" {
 			err := msghandlers.HandleDeleteVideo(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
 
 		if update.Message.Text == "Get all users" {
 			err := msghandlers.HandleGetAllUsers(update, bot)
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		}
--- go.mod
+++ go.mod
@@ -6,6 +6,14 @@ require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.1.0 // indirect
+	github.com/j0n1que/sso-protos v0.0.6 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/grpc v1.68.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 )
